d791f7774fd35ed49ead211958b4e5d8b8f7a062
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-02-22 18:27:23 -0300

    js

---
+++ aula039/index.js
@@ -0,0 +1,18 @@
+// let i = 0;
+
+// while(i <=10){
+//     console.log(i);
+//     i++;
+// }
+
+function max(x, y) {
+    return x > y ? x : y;
+}
+
+const max2 = (x, y) => {
+    return x > y ? x : y;
+};
+
+const max3 = (x, y) => x > y ? x : y;
+
+console.log(max3(1, 2));
