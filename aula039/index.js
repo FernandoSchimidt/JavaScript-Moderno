d0d070898de87532f4673b1b7c8d027d4a46d220
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-02-26 09:02:20 -0300

    s
    s

--- aula039/index.js
+++ aula039/index.js
@@ -1,18 +1,35 @@
-// let i = 0;
+// // let i = 0;
 
-// while(i <=10){
-//     console.log(i);
-//     i++;
+// // while(i <=10){
+// //     console.log(i);
+// //     i++;
+// // }
+
+// function max(x, y) {
+//     return x > y ? x : y;
 // }
 
-function max(x, y) {
-    return x > y ? x : y;
+// const max2 = (x, y) => {
+//     return x > y ? x : y;
+// };
+
+// const max3 = (x, y) => x > y ? x : y;
+
+// console.log(max3(1, 2));
+
+function max4(x, y, z) {
+    if (typeof x || y || z != 'number') {
+        return 'Algum dos valores não é um numero!'
+    } else if (x > y && x > z) {
+        return x;
+    } else if (y > z) {
+        return y;
+    } else {
+        return z;
+    }
 }
+console.log(max4(15.55551, 'fer',5));
 
-const max2 = (x, y) => {
-    return x > y ? x : y;
-};
+// const maior = Math.round(Math.random() * 100);
 
-const max3 = (x, y) => x > y ? x : y;
-
-console.log(max3(1, 2));
+ // console.log(maior);
