aa4c0622dc3c094454d06323e7b427cfc49c9f31
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-10 00:10:55 -0300

    s

--- aulla009/index.js
+++ aulla009/index.js
@@ -1,9 +1,29 @@
-//ECMAScript padronizacao da linguagem javascript
-var nome = 'Fernando';
-console.log(nome);
+// //ECMAScript padronizacao da linguagem javascript
+// var nome = 'Fernando';
+// console.log(nome);
 
-//let não pode ser  redeclarada
+// //let não pode ser  redeclarada
 
-//Nao fazer isso
-nome = 'Schimidt'
-console.log(nome)
+// //Nao fazer isso
+// nome = 'Schimidt'
+// console.log(nome)
+
+const conta = function(operador, acumulador, ...numeros) {
+    for (let numero of numeros) {
+        if (operador === '+') {
+            acumulador += numero;
+        }
+        if (operador === '-') {
+            acumulador -= numero;
+        }
+        if (operador === '*') {
+            acumulador *= numero;
+        }
+        if (operador === '/') {
+            acumulador = numero;
+        }
+    }
+
+    console.log(acumulador)
+};
+conta('+', 0, 20, 30, 40, 50);
