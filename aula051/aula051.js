865eeba8e1e6e5ef98044ec296ca06e7f2053459
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-11 23:26:49 -0300

    s

--- aula051/aula051.js
+++ aula051/aula051.js
@@ -1,11 +1,26 @@
 // 
 
-function falaFrase(comeco) {
-    function falaResto(resto) {
-        return comeco + ' ' + resto;
-    }
-    return falaResto;
+// function falaFrase(comeco) {
+//     function falaResto(resto) {
+//         return comeco + ' ' + resto;
+//     }
+//     return falaResto;
+// }
+
+// const olaMundo = falaFrase('Hello');
+// console.log(olaMundo('Mundo'));
+
+function criaMultiplicador(multiplicador) {
+    //multiplicador
+    return function(n) {
+        return n * multiplicador;
+    };
 }
 
-const olaMundo = falaFrase('Hello');
-console.log(olaMundo('Mundo'));
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
---
+++ aula052/aula052.js
@@ -0,0 +1,10 @@
+const nome = 'Fernando';
+
+function falaNome() {
+    console.log(nome);
+}
+
+function usaFalaNome() {
+    falaNome();
+};
+usaFalaNome();
