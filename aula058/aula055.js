f0f304598ae7fd0b84814a73ce114af600748f2f
=fernando schimidt <=>
2020-05-02 17:08:00 -0300

    s
    s

--- aula058/aula055.js
+++ aula058/aula055.js
@@ -1,13 +1,17 @@
 (function (idade, peso, altura) {
 
     const sobrenome = 'Schimidt';
+
     function falaOi(nome) {
         return nome + ' ' + sobrenome;
     }
+
     function falaNome() {
         console.log(falaOi('Fernando'));
     }
 
     falaNome()
-    console.log(idade,peso,altura)
-})(30,100,1.75);
+    console.log(idade, peso, altura)
+})(30, 100, 1.75);
+
+
--- aula058/aula058.js
+++ aula058/aula058.js
@@ -1,1 +1,19 @@
+//Função construtora
+function Pessoa(nome, sobrenome) {
 
+    const Id= 123456
+
+    const metodoInterno = function(){
+        
+    }
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.metodo = function () {
+        console.log('Sou um metodo')
+    }
+}
+
+const p1 = new Pessoa('Fernando', 'Schimidt')
+
+console.log(p1)
+p1.metodo()
