01f5c3f66a326d3cd7ca9107c7e64f4e33c4307a
=fernando schimidt <=>
2020-05-18 20:59:11 -0300

    api
    api

---
+++ API/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-console": "off",
+    "class-methods-use-this": "off",
+    "import-first": "off"
+  },
+};
---
+++ API/app.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express from 'express'
+import homeRoutes from './src/routes/homeRoute';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewars();
+    this.routes();
+  }
+  middlewars() {
+    this.app.use(express.urlencoded({
+      extended: true
+    }));
+    this.app.use(express.json());
+  }
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
